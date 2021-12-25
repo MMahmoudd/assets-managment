80795e7fee21ed7ccb20832a7eca6bdb2c805033
Mahmoud Mostafa <mahmoud.mostafa1008@gmail.com>
2021-12-25 15:30:00 +0200

    complete edit on export assets and add erp code

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://81.29.108.94/Asset_Api_Prod/api'
-export const API_URL = 'https://81.29.108.94/Assets_Api/api'
+export const API_URL = 'http://81.29.108.94/Asset_Api_Prod/api'
+// export const API_URL = 'https://81.29.108.94/Assets_Api/api'
