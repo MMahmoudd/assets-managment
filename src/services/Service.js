da6e0ce047f2dfc03ca3a9f39276e6a49f0700c0
MMahmoudd <mahmoud.mostafa1008@gmail.com>
2022-02-10 16:01:33 +0200

    add interceptors in the project to check on auth for user

--- src/services/Service.js
+++ src/services/Service.js
@@ -8,7 +8,7 @@ _axios.interceptors.response.use((response) => {
       return response
   },
   function (error) {
-      if (error.response.data.statusCode === 401) {
+      if (error.response.status === 401) {
           localStorage.removeItem('token')
           window.location = '/login'
       }
